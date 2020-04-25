242b92c84a1b01249f844f73ae9aaf986018d265
1337erik <erikpwhite@gmail.com>
2020-04-25 10:39:54 -0400

    added website template - no fixing up yet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,29 +2,34 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'MetaLevel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    {{-- <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @include( 'partials.templatecss' )
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 </head>
 <body>
-    <div id="app">
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    @include( 'partials.topnav' )
+
+    @yield('content')
+
+    @include( 'partials.footer' )
+
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+    @include( 'partials.templatejs' )
 </body>
 </html>
---
+++ resources/views/partials/templatecss.blade.php
@@ -0,0 +1,22 @@
+<link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:100,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Styles -->
+
+<link rel="stylesheet" href="{{ asset('css/open-iconic-bootstrap.min.css' ) }}">
+<link rel="stylesheet" href="{{ asset('css/animate.css' ) }}">
+
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css' ) }}">
+<link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css' ) }}">
+<link rel="stylesheet" href="{{ asset('css/magnific-popup.css' ) }}">
+
+<link rel="stylesheet" href="{{ asset('css/aos.css' ) }}">
+
+<link rel="stylesheet" href="{{ asset('css/ionicons.min.css' ) }}">
+
+<link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker.css' ) }}">
+<link rel="stylesheet" href="{{ asset('css/jquery.timepicker.css' ) }}">
+
+
+<link rel="stylesheet" href="{{ asset('css/flaticon.css' ) }}">
+<link rel="stylesheet" href="{{ asset('css/icomoon.css' ) }}">
+<link rel="stylesheet" href="{{ asset('css/style.css' ) }}">
